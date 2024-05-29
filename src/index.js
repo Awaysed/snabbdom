608a46e10339e25aca89de3eb9814dc742132648
Awaysed <122900171@qq.com>
2024-05-29 23:35:32 +0800

    add:createElement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import patch from './my-snabbdom/patch'
 import h from './my-snabbdom/h'
 var container = document.getElementById('container')
+console.log(333,container);
 const myVonde = h('h1',{},'你好')
 patch(container,myVonde)
--- src/my-snabbdom/createElement.js
+++ src/my-snabbdom/createElement.js
@@ -1,6 +1,12 @@
  // 真正创建节点
 
  export default function(vnode,pivot){
+    console.log(2222,pivot,pivot.parentNode);
+    let domNode = document.createElement(vnode.sel)
+    if(vnode.text != '' && (vnode.children == undefined || vnode.children.length == 0)){
+        domNode.innerText = vnode.text
+        pivot.parentNode.insertBefore(domNode,pivot)
+    }else if(Array.isArray(vnode.children) && vnode.children.length > 0){
 
+    }
  }
- 
