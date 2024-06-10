aeb794beeb06973699bc7bd96e04d1ddf2c6b4fe
Awaysed <122900171@qq.com>
2024-06-10 22:57:48 +0800

    add:key

--- src/my-snabbdom/vnode.js
+++ src/my-snabbdom/vnode.js
@@ -1,6 +1,7 @@
 export default function (sel,data,children,text,elm) {
+    let key = data?.key || undefined 
     return {
-        sel,data,children,text,elm
+        sel,data,children,text,elm,key
     }
 } 
 // 虚拟节点的属性
